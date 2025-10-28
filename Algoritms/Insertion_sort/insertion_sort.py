2f1f77bb4a462499192a2a8e1a50a275b80e3fde
Ivan K <kosi17100@gmail.com>
2025-10-28 20:25:53 +0300

    добавлен код сортировки простыми вставками, тесты и описание

--- Algoritms/Insertion_sort/insertion_sort.py
+++ Algoritms/Insertion_sort/insertion_sort.py
@@ -1,5 +1,17 @@
 
 
 
-def insertion_sort():
-    pass
+def insertion_sort(lst: list, direction=lambda x,y: x<y):
+
+    if not isinstance(lst, list):
+        raise ValueError(' не является массивом')
+    for i in range(1, len(lst)):
+        iterator = lst[i]
+        j = i - 1
+        while direction(iterator, lst[j]) and j>=0:
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+            j-=1
+    return lst
+
+app = [0,1,3,2,5]
+print(insertion_sort(app))
